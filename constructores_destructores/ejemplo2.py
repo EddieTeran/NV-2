4b3f943dad8212103c3bcdeb89c56ac587341920
Eddie Teran <ep.teranq@uea.edu.ec>
2024-07-08 23:45:07 -0500

    cometarios añadidos

--- constructores_destructores/ejemplo2.py
+++ constructores_destructores/ejemplo2.py
@@ -1,12 +1,12 @@
 class Auto:
-    def __init__(self, nombre):
-        self.nombre = nombre
+    def __init__(self, nombre): # Constructor: Se llama cuando se crea una instancia de la clase.
+        self.nombre = nombre  # Inicializa los atributos del objeto
         print(f" Inicializando clase Auto {self.nombre}.")
 
-    def __del__(self):
+    def __del__(self): # Destructor: Se llama cuando una instancia está a punto de ser destruida.
         print(f" Clase {self.nombre} borrada.")
 
-dest2 = Auto("")
+dest2 = Auto("") # Crear una instancia de la clase
 del  dest2
 
               
