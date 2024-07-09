8e552955be024252893e92596636b76393ee33da
Eddie Teran <ep.teranq@uea.edu.ec>
2024-07-08 23:28:07 -0500

    Comentarios

--- constructores_destructores/ejemplo1.py
+++ constructores_destructores/ejemplo1.py
@@ -1,13 +1,14 @@
 class Agenda:
-    def __init__(self, nombre):
-        self.nombre = nombre
+
+    def __init__(self, nombre): # Constructor: Se llama cuando se crea una instancia de la clase.
+        self.nombre = nombre # Inicializa los atributos del objeto.
         print(f"Inicializando agenda {self.nombre}.")
 
-    def __del__(self):
-        print(f" Agenda {self.nombre} borrada.")
+    def __del__(self):# Destructor: Se llama cuando una instancia está a punto de ser destruida.
+        print(f" Agenda {self.nombre} destruida.")
 
 # Crear una instancia de la clase
-dest1 = Agenda("")
+dest1 = Agenda("")  # Crear una instancia de la clase
 
 # Eliminar la instancia (esto invocará el destructor)
 del dest1
