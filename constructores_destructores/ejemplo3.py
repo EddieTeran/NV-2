ada1204e1d507de3bc2a267bf44eecacb7900a0b
Eddie Teran <ep.teranq@uea.edu.ec>
2024-07-08 23:46:08 -0500

    comentarios añadidos

--- constructores_destructores/ejemplo3.py
+++ constructores_destructores/ejemplo3.py
@@ -1,19 +1,19 @@
 class Matriz:
-    def __init__(self, nombre, fecha):
-        self.nombre = nombre
+    def __init__(self, nombre, fecha): # Constructor de la clase Matriz: Se llama cuando se crea una instancia.
+        self.nombre = nombre # Inicializa los atributos
         self.fecha = fecha
         print(f"Inicializando Clase matriz {self.fecha}.")
 
-class Sucursal1(Matriz):
-    def __init__(self, nombre, fecha, codigo, ciudad):
+class Sucursal1(Matriz): # Llamada al constructor de la superclase (Matriz)
+    def __init__(self, nombre, fecha, codigo, ciudad): # Inicializa los atributos adicionales 'codigo' y 'ciudad'.
         super().__init__(nombre, fecha)
         self.codigo = codigo
         self.ciudad = ciudad
         print(f"Inicializando subclase {self.nombre}.")
 
-    def __del__(self):
+    def __del__(self): # Destructor de la subclase: Se llama cuando una instancia está a punto de ser destruida.
         print(f"Subclase {self.nombre} borrada.")
 
 
-dest3 = Sucursal1("", "","","")
-del dest3
+dest3 = Sucursal1("", "","","") # Crear una instancia de la subclase Sucursal1
+del dest3 # Eliminar la instancia (esto invocará el destructor)
